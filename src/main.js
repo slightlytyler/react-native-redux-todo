6d8c26c46c77937d8590117a26ce9aeaf00cea84
Tyler Martinez <slightlytyler@gmail.com>
2015-09-28 18:42:08 -0500

    Init from boiler plate

---
+++ src/main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import React from 'react-native'
+import App from './containers/app'
+
+var {
+  AppRegistry
+} = React
+
+AppRegistry.registerComponent('react_native_starter', () => App)
