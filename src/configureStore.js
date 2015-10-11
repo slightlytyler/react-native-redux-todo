17d163e0bd215420990e1e8d7012797f3470bd2d
Tyler Martinez <slightlytyler@gmail.com>
2015-10-11 18:43:50 -0500

    async storage working

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,15 +1,34 @@
 'use strict'
 
-import appReducers from './reducers/index'
-import thunk from 'redux-thunk'
 import { compose, createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import storage from 'redux-storage'
+import createEngine from 'redux-storage/engines/reactNativeAsyncStorage';
+
+import appReducers from 'reducers/index'
+import { setState } from 'pods/todos/actions'
 
 export default function configureStore(initialState) {
-  // // Enables your middleware:
-  let composers = [ applyMiddleware(thunk) ]
-  const finalCreateStore = compose(...composers)(createStore)
+  // Setup storage
+  const reducer = storage.reducer(appReducers);
+  var engine = createEngine('my-save-key');
+      engine = storage.decorators.filter(engine, [
+        ['todosReducer', 'todos', 'list']
+      ]);
+      engine = storage.decorators.immutablejs(engine, [
+        ['todosReducer']
+      ]);
 
-  let store = finalCreateStore(appReducers, initialState)
+  const storageMiddleware = storage.createMiddleware(engine);
+
+  // Enables your middleware:
+  let composers = [
+    applyMiddleware(thunk),
+    applyMiddleware(storageMiddleware)
+  ];
+  const finalCreateStore = compose(...composers)(createStore);
+
+  let store = finalCreateStore(reducer, initialState);
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('./reducers/index', () => {
@@ -17,7 +36,11 @@ export default function configureStore(initialState) {
       store.replaceReducer(nextRootReducer)
     })
   }
-  return store
+  const load = storage.createLoader(engine);
+
+  load(store).then((loadedState) => store.dispatch(setState(loadedState)));
+
+  return store;
 }
 
 // include devtools and persiststate if enabled
