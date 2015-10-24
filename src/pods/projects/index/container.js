c428e5189e98af2cf079ef7d539001c99254ecca
Tyler Martinez <slightlytyler@gmail.com>
2015-10-24 18:18:47 -0500

    Better layout for project.index container

--- src/pods/projects/index/container.js
+++ src/pods/projects/index/container.js
@@ -6,40 +6,38 @@ import { createSelector } from 'reselect';
 import ProjectsIndexComponent from './component'
 
 const ProjectsIndexContainer = connect(state => {
-  const projectsSelector = state => state.projects.entities;
-  const todosSelector = state => state.todos.entities;
-
-
-  const todosByProjectSelector = createSelector(
-    projectsSelector,
-    todosSelector,
-    (projects, todos) => _.mapValues(projects, project =>
-      _.pick(todos, todo =>
-        todo.project === project.id
-      )
-    )
-  );
-
-  const projectsWithCompleteSelector = createSelector(
-    projectsSelector,
-    todosByProjectSelector,
-    (projects, todosByProject) => _.mapValues(projects, project => {
-      let todos = todosByProject[project.id];
-
-      let complete = !_.isEmpty(todos) && _.every(todos, todo =>
-        todo.complete
-      );
-
-      return Object.assign({}, project, {
-        complete: complete
-      });
-    })
-  );
-
-
   return {
     projects: projectsWithCompleteSelector(state)
   };
 })(ProjectsIndexComponent);
+
+const projectsSelector = state => state.projects.entities;
+const todosSelector = state => state.todos.entities;
+
+const todosByProjectSelector = createSelector(
+  projectsSelector,
+  todosSelector,
+  (projects, todos) => _.mapValues(projects, project =>
+    _.pick(todos, todo =>
+      todo.project === project.id
+    )
+  )
+);
+
+const projectsWithCompleteSelector = createSelector(
+  projectsSelector,
+  todosByProjectSelector,
+  (projects, todosByProject) => _.mapValues(projects, project => {
+    let todos = todosByProject[project.id];
+
+    let complete = !_.isEmpty(todos) && _.every(todos, todo =>
+      todo.complete
+    );
+
+    return Object.assign({}, project, {
+      complete: complete
+    });
+  })
+);
 
 export default ProjectsIndexContainer;
